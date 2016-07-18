9669cc22121df51c6d7fff50bad300844ca60ad4
carlbona <carl.bonander@gmail.com>
2016-07-18 17:19:03 +0200

    Functions to cache an inverted square matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions to cache results after inversion of a square matrix,
+## exploiting the lexical scoping rules in R. 
 
-## Write a short comment describing this function
+## This is a function to create a special matrix object that can cache
+## the results produced by the cacheSolve function below. Takes a matrix as an argument.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setSolve <- function(solve) m <<- solve
+        getSolve <- function() m
+        list(set = set, get = get, 
+             setSolve = setSolve,
+             getSolve = getSolve)
+        
 }
 
 
-## Write a short comment describing this function
+## Invert a square matrix, using cached results if available. If no results are available,
+## it uses the solve() function to calculate 
+## Takes an object created by makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getSolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setSolve(m)
+        m
 }
